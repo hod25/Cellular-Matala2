248430b06e2e5955ab2ffc91046639bb15e18818
hod25 <hod7112000@gmail.com>
2024-12-22 22:08:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cellular_matala2/StudentsListFragment.kt
@@ -0,0 +1,56 @@
+package com.example.cellular_matala2
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cellular_matala2.adapter.StudentsRecyclerAdapter
+import com.example.cellular_matala2.model.Student
+import com.example.cellular_matala2.model.Model
+
+class StudentsListFragment : Fragment() {
+
+    private var students: MutableList<Student>? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_students_list, container, false)
+
+        students = Model.shared.students
+        val recyclerView: RecyclerView = view.findViewById(R.id.students_list_activity_recycler_view)
+        recyclerView.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(context)
+        recyclerView.layoutManager = layoutManager
+
+        val adapter = StudentsRecyclerAdapter(students)
+
+        adapter.listener = object : OnItemClickListener {
+            override fun onItemClick(position: Int) {
+                Log.d("TAG", "On click Activity listener on position $position")
+            }
+
+            override fun onItemClick(student: Student?) {
+                Log.d("TAG", "On student clicked name: ${student?.name}")
+
+                // Navigate to AddStudentActivity
+                val intent = Intent(requireContext(), AddStudentActivity::class.java)
+                intent.putExtra("student_name", student?.name)
+                intent.putExtra("student_id", student?.id)
+                startActivity(intent)
+            }
+        }
+
+        recyclerView.adapter = adapter
+
+        return view
+    }
+}
---
+++ app/src/main/java/com/example/cellular_matala2/model/Student.kt
@@ -0,0 +1,8 @@
+package com.example.cellular_matala2.model
+
+data class Student(
+    val name: String,
+    val id: String,
+    val avatarUrl: String,
+    var isChecked: Boolean
+)
