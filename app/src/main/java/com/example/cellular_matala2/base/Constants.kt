3fb787cc605b06acfc12b55f7c82569513301fbc
hod25 <hod7112000@gmail.com>
2025-01-04 21:07:09 +0200

    logo

---
+++ app/src/main/java/com/example/cellular_matala2/base/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.cellular_matala2.base
+
+import com.example.cellular_matala2.model.Student
+
+typealias StudentsCallback = (List<Student>) -> Unit
+typealias EmptyCallback = () -> Unit
+
+object Constants {
+
+}
