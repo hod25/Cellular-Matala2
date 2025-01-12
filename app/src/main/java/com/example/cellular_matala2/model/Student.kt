fea4ad7d0ec998b3e620e50b82f57b550c1f45b1
hod25 <hod7112000@gmail.com>
2025-01-12 11:47:38 +0200

    Students and vcs

--- app/src/main/java/com/example/cellular_matala2/model/Student.kt
+++ app/src/main/java/com/example/cellular_matala2/model/Student.kt
@@ -1,5 +1,6 @@
 package com.example.cellular_matala2.model
 
+import android.location.Address
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -8,5 +9,7 @@ data class Student(
     @PrimaryKey val id: String,
     val name: String,
     val avatarUrl: String,
+    val phone : String,
+    val address : String,
     var isChecked: Boolean
 )
